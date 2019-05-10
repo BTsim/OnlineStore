7ebb6209b1fece7549b8b2af1a8fe3afad370b26
Bahdan Tsimashevich <timoh.manutd@gmail.com>
2019-05-10 11:57:09 +0300

    Completed homewrk_advanced 1 (online store)

---
+++ OnlineStore/Product.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore
+{
+    class Product
+    {
+        public string ProductName { get; set; }
+        public Int32 ProductId { get; set; }
+        public double ProductPrice { get; set; }
+
+        public void PriceChecker(Product product1, Product product2)
+        {
+            if (product1 > product2)
+            {
+                Console.WriteLine($"{product1}>{product2}");
+            }
+            else if (product1 == product2)
+            {
+                Console.WriteLine($"{product1}={product2}");
+            }
+            else
+            {
+                Console.WriteLine($"{product1}<{product2}");
+            }
+        }
+
+        public static bool operator ==(Product prod1, Product prod2)
+        {
+            if (prod1.ProductPrice == prod2.ProductPrice)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+        public static bool operator !=(Product prod1, Product prod2)
+        {
+            if (prod1.ProductPrice != prod2.ProductPrice)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator >(Product prod1, Product prod2)
+
+        {
+            return prod1.ProductPrice > prod2.ProductPrice;
+        }
+
+        public static bool operator <(Product prod1, Product prod2)
+        {
+            return prod1.ProductPrice < prod2.ProductPrice;
+        }
+    }
+}
+}
---
+++ OnlineStore/Sale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore
+{
+    class Sale
+    {
+        public int SalePercent { get; set; }
+        public List<Product> DiscountProducts { get; set; }
+    }
+}
